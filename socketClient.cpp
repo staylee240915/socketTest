92346bf9b554d6af7518b6b687a38910577d7c6e
staylee0915 <staylee0915@gmail.com>
2025-03-03 21:44:44 +0900

    update socket server code

--- socketClient.cpp
+++ socketClient.cpp
@@ -4,25 +4,31 @@
 #include <sys/socket.h>
 #include <unistd.h>
 #include <stdio.h>
+#include <arpa/inet.h>
 
 using namespace std;
 
 int main(void){
 
+    //ipv4와 tcp를 이용한 소켓client 생성
     int clientSocket = socket(AF_INET, SOCK_STREAM,0);
 
     sockaddr_in serverAddress;
-    serverAddress.sin_family = AF_INET;
-    serverAddress.sin_port = htons(9032);
-    serverAddress.sin_addr.s_addr = INADDR_ANY;
+    serverAddress.sin_family = AF_INET; //ipv4
+    serverAddress.sin_port = htons(9032); //9032 포트
+    serverAddress.sin_addr.s_addr = inet_addr("127.0.0.1"); //연결받을 주소
 
+    //소켓 연결 시작.
     connect(clientSocket,(struct sockaddr*)&serverAddress, sizeof(serverAddress));
 
+    //데이터 입력
     char buf[100]={""};
     fgets(buf,sizeof(buf), stdin);
     
+    //데이터 전달. -> buff 부분에서 protocol 정의하여, 전송할 내용 전달할 것.
     send(clientSocket,buf,strlen(buf),0);
     
+    //전송 종료.
     close(clientSocket);
 
     return 0;
--- socketServer.cpp
+++ socketServer.cpp
@@ -2,8 +2,15 @@
 #include <iostream>
 #include <netinet/in.h> //인터넷 프로토콜 (ipv4관련 구조체와 상수 정의 헤더)
 #include <sys/socket.h> //소켓프로그래밍을 위한 기본적인 함수와 구조체 상수 정의
-#include <unistd.h>
+#include <sys/select.h> //select를 통한 멀티플렉싱 사용
+#include <unistd.h> //close()위함
+#include <vector>
+
 using namespace std;
+
+const int port = 9032;
+const int maxConn = SOMAXCONN;
+#define BUF_SIZE 128;
 
 int main(void){
 
@@ -13,25 +20,53 @@ int main(void){
     //2. 소켓 주소 지정
     sockaddr_in serverAddress;
     serverAddress.sin_family = AF_INET;
-    serverAddress.sin_port = htons(9032);
-
-    //연결을 받을 주소
+    serverAddress.sin_port = htons(port);
+    //ip 주소가 무엇인지 신경쓰지 않는다 (*ip주소는 nic 카드 갯수에 따라서 달라지므로)
     serverAddress.sin_addr.s_addr = INADDR_ANY;
 
+
     //socket 바인딩.
-    if(bind(serverSocket, (struct sockaddr*) &serverAddress, sizeof(serverAddress))<0){
-        perror("bind error");
+    int isBinding = ::bind(serverSocket, (struct sockaddr*)&serverAddress, sizeof(serverAddress));
+
+    if(isBinding <0){
+        perror("bind error : 연결종료");
+        close(serverSocket);
+        return 0;
     };
 
-    listen(serverSocket,5);
+    //접속대기
+    int isListen = listen(serverSocket,maxConn);
+    if(isListen == -1){
+        puts("ERROR : 접속대기 불가");
+        return 0;
+    }
+
+    sockaddr addrInfo;
+    socklen_t addrLen;
 
     int clientSocket
-        = accept(serverSocket,nullptr,nullptr);
-    
-    char buffer[1024] = {0};
-    recv(clientSocket,buffer,sizeof(buffer),0);
-    cout<<"message from client "<<clientSocket<<" : "<< buffer<<"\n";
+        = accept(serverSocket, (struct sockaddr*)&addrInfo, &addrLen);
+    while(clientSocket != -1){
+        puts("accept : 새로운 client 연결");
+        //연결시 연결정보에 대한 내용을 추후 로그로 기록 남길 예정.
+    }
 
+    char buffer[128] = {0};
+    
+    int nRecv = recv(clientSocket,buffer,sizeof(buffer),0);
+    while(nRecv != -1){
+        //수신한 문자열을 그대로 client에 반환하여 전송.
+        send(clientSocket,buffer,sizeof(buffer),0);
+        cout<<"message from client"<<clientSocket<<"\n";
+        puts(buffer);
+        //메모리 초기화.
+        memset(buffer,0,sizeof(buffer));
+
+        //client에 shutdown 읽기버퍼 쓰기버퍼 모두 종료.
+        shutdown(clientSocket,SHUT_RDWR);
+        puts("connection closed.");
+    }
+    //리슨 소켓 닫기
     close(serverSocket);
 
     return 0;
