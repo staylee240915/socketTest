a623c7890516665ce61ecf6072a66568b0c6c431
staylee0915 <staylee0915@gmail.com>
2025-03-09 17:29:54 +0900

    update flush

--- socketClient.cpp
+++ socketClient.cpp
@@ -23,24 +23,39 @@ int main(void){
     serverAddress.sin_addr.s_addr = inet_addr(serverADDR); //연결받을 주소
 
     //소켓 연결 시작.
+    //connection이 중간에 서버 다운으로 끊어졌을때, conn값이 계속 유지되어 client가 꺼지지 않고
+    //server가 재기동 되지 않는 문제가 존재.
     int isConn = connect(clientSocket,(struct sockaddr*)&serverAddress, sizeof(serverAddress));
     if(isConn ==-1){
         //에러코드 반환 (추후 에러관련 내용을 에러 코드를 반환하고, 로그에 저장하도록 개선.)
-        puts("ERROR : 서버와 연결할 수 없습니다.");
+        puts("ERROR : 서버와 연결할 수 없습니다."); fflush(stdout);
         return 0;
     }
     //데이터 입력
     char buf[128]={0};
+    sockaddr addrInfo;
+    socklen_t addrLen;
+
     while(1){
         //입력
-        fgets(buf,sizeof(buf), stdin);
-        if(strcmp(buf,"EXIT")==0) break;
+        fgets(buf,sizeof(buf), stdin); fflush(stdin);
+        buf[strcspn(buf, "\n")] = '\0';
+        if(strcmp(buf,"EXIT")==0){
+            memset(buf,0,sizeof(buf));
+            break;
+        }
         //데이터 전달. -> buff 부분에서 protocol 정의하여, 전송할 내용 전달할 것.
-        send(clientSocket,buf,strlen(buf)+1,0);
+        send(clientSocket,buf,strlen(buf),0);
+        //버퍼 초기화.
+        memset(buf,0,sizeof(buf));
+        
+        recv(clientSocket,buf,sizeof(buf),0);
+        cout<<"message from server : ";
+        puts(buf);
         memset(buf,0,sizeof(buf));
     }
     //client에서 shutdown 전송.
-    shutdown(clientSocket,SHUT_RDWR);
+    //shutdown(clientSocket,SHUT_RDWR);
     //전송 종료.
     close(clientSocket);
 
--- socketServer.cpp
+++ socketServer.cpp
@@ -23,7 +23,10 @@ int main(void){
     serverAddress.sin_port = htons(port);
     //ip 주소가 무엇인지 신경쓰지 않는다 (*ip주소는 nic 카드 갯수에 따라서 달라지므로)
     serverAddress.sin_addr.s_addr = INADDR_ANY;
-
+    int nRecv = 0;
+    int clientSocket =0;
+    sockaddr addrInfo;
+    socklen_t addrLen;
 
     //socket 바인딩.
     int isBinding = ::bind(serverSocket, (struct sockaddr*)&serverAddress, sizeof(serverAddress));
@@ -41,31 +44,29 @@ int main(void){
         return 0;
     }
 
-    sockaddr addrInfo;
-    socklen_t addrLen;
-
-    int clientSocket
-        = accept(serverSocket, (struct sockaddr*)&addrInfo, &addrLen);
-    while(clientSocket != -1){
-        puts("accept : 새로운 client 연결");
+    while((clientSocket = accept(serverSocket, (struct sockaddr*)&addrInfo, &addrLen))!=-1){
+        puts("accept : 새로운 client 연결"); fflush(stdout);
         //연결시 연결정보에 대한 내용을 추후 로그로 기록 남길 예정.
 
         char buffer[128] = {0};
     
-        int nRecv = recv(clientSocket,buffer,sizeof(buffer),0);
-        while(nRecv != -1){
+        while((nRecv=recv(clientSocket,buffer,sizeof(buffer),0))!=-1){
             //수신한 문자열을 그대로 client에 반환하여 전송.
             send(clientSocket,buffer,sizeof(buffer),0);
+
             cout<<"message from client"<<clientSocket<<"\n";
+            //메세지 표출
             puts(buffer);
             //메모리 초기화.
             memset(buffer,0,sizeof(buffer));
         }
         //client에서 shutdown 읽기버퍼 쓰기버퍼 모두 종료.
         shutdown(serverSocket,SHUT_RDWR);
+        
+        //서버와 연결된 클라이언트의 소켓 close;
         close(clientSocket);
 
-        puts("connection closed.");
+        puts("connection closed."); fflush(stdout);
     }
     //리슨 소켓 닫기
     close(serverSocket);
